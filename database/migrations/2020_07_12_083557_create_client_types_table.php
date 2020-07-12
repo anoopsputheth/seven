21e40403f39397b1f175da1b5ae170cda7846465
Anoop PS <anoopsputheth@gmail.com>
2020-07-12 14:08:03 +0530

    Commit 2.12.07.2020

---
+++ database/migrations/2020_07_12_083557_create_client_types_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('client_types', function (Blueprint $table) {
+           
+            $table->bigIncrements('id');
+            
+            $table->string('name', 100)->unique();
+            $table->string('description')->nullable();
+
+            $table->timestamps();
+            $table->softDeletesTz('deleted_at');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('client_types');
+    }
+}
